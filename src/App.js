32ef6efd8f83aa1d9802bf6f6fcc6ab1bdfc9494
shibil102 <shibilmohamed9@gmail.com>
2020-11-02 21:50:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useState} from 'react';
+import Home from './Components/Home'
+import './App.css'
+import About from './Components/About';
+import Htp from './Components/Htp';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Playname from './Components/Playname';
+import PlayBoard from './Components/PlayBoard';
+import {Player} from './Components/Context'
+import Music from './Components/Music'
+const App = () => {
+  const [player1,setPlayer1] = useState('Player1');
+  const [player2,setPlayer2] = useState('Player2');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Music/>
+       <BrowserRouter>
+       <Player.Provider value={{player1,setPlayer1,player2,setPlayer2}}>
+      <Route exact path="/">
+      <Home/>  
+      </Route>
+      
+      <Route path = "/about">
+        <About/>
+      </Route>
+      
+      <Route path = "/htp">
+        <Htp/>
+      </Route>
+      <Route path = "/play">
+      <Playname/>
+    </Route>
+    <Route path = "/game">
+      <PlayBoard/>
+    </Route>
+    </Player.Provider>
+      </BrowserRouter>
     </div>
   );
-}
-
+};
 export default App;
